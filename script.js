dce787f008e53cb855046bc450a07f8b63914991
KAcper <kacperkolano04@gmail.com>
2022-07-01 21:43:05 +0200

    Just testing

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const DEFAULT_COLOR = '#FCEDDA'
 const DEFAULT_SIZE = 16
 const DEFAULT_MODE = 'color'
 
+/*
 const grid = document.querySelector(".grid-content")
 
 let color = 'black'
@@ -68,5 +69,77 @@ window.onload = () =>
 }
 
 //DOWNLOAD PICTURE -> Alpha Testing -> Maybe be deleted in future
+const download = document.querySelector('#download')
 
-// for now nothing xd
+function doCapture() {
+    html2canvas(document.getElementById('download').then(function (canvas) {
+        console.log(canvas.toDataURL("image/jpeg", 0.9))
+    }))
+}
+
+doCapture()
+*/
+
+const canvas = document.getElementById('canvas')
+
+const ctx = canvas.getContext('2d');
+
+
+
+/*let canvasBounds = canvas.getBoundingClientRect();
+
+let coord = 
+{
+    x: 0,
+    y: 0,
+    lastX: 0,
+    lastY: 0
+}
+
+document.addEventListener('mousedown', start);
+document.addEventListener('mouseup', stop);
+
+window.addEventListener('resize', resize);
+
+resize()
+
+function resize()
+{
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+}
+
+function reposition(event)
+{
+    let bounds = canvas.getBoundingClientRect();
+    coord.x = event.pageX - bounds.left - scrollX;
+    coord.y = event.pageY - bounds.top - scrollY;
+    coord.x /= bounds.width;
+    coord.y /= bounds.height;
+
+    coord.x *= canvas.width;
+    coord.y *= canvas.height;
+}
+
+function start(event)
+{
+    document.addEventListener('mousemove', draw);
+    reposition(event);
+}
+
+function stop()
+{
+    document.removeEventListener('mousemove', draw);
+}
+
+function draw(event)
+{
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = "black";
+    ctx.moveTo(coord.x, coord.y);
+    reposition(event);
+    ctx.lineTo(coord.x, coord.y);
+    ctx.stroke();
+}*/
